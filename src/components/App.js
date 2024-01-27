a8cc28e2770bc3354082a80198df9526bd810d43
Garaisourav12 <garaisourav12@gmail.com>
2024-01-28 05:00:38 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <div id="main">
         {/* Do not remove the main div */}
+        <BrowserRouter>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+          </ul>
+
+          <Routes>
+            <Route path="/" element={<h1 id="home">Welcome to my website!</h1>} />
+            <Route path="/about" element={
+              <>
+                <h1>About</h1>
+                <p>This is a simple React Router program.</p>
+              </>
+            } />
+          </Routes>
+        </BrowserRouter>
     </div>
   )
 }
